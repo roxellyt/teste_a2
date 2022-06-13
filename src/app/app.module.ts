9eedf76b1c2c77e1b058c4122be18e525bc3c641
roxellyt <roxelly.tavares@gmail.com>
2022-06-13 20:09:49 -0300

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TaskComponent } from './task/task.component';
+import { TasksService } from './tasks.service';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 @NgModule({
   imports: [
@@ -19,5 +22,17 @@ import { TaskComponent } from './task/task.component';
   ],
   declarations: [AppComponent, HelloComponent, HomeComponent, TaskComponent],
   bootstrap: [AppComponent],
+  providers: [TasksService],
 })
 export class AppModule {}
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYeyAq8TM66zyw7yQWikq2J0Ax6HU607Y',
+  authDomain: 'prja2-35089.firebaseapp.com',
+  databaseURL: 'https://prja2-35089-default-rtdb.firebaseio.com',
+  projectId: 'prja2-35089',
+  storageBucket: 'prja2-35089.appspot.com',
+  messagingSenderId: '801804196078',
+  appId: '1:801804196078:web:f10af245b8ff2df04c3bff',
+  measurementId: 'G-X4DESZPTVB',
+};
